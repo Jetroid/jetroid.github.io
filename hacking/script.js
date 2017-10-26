31839c46698ac51915b52b4457fc7c7f3605c9ba
Jetroid <holtjet@gmail.com>
2017-10-26 13:58:41 +0100

    Clean up Hacking script a little

--- hacking/script.js
+++ hacking/script.js
@@ -17,7 +17,7 @@ var viewPost = function(postID){
 	//The second nested child is the one which scrolls - reset it to the top
 	var scrollingElement = document.getElementById(postID).firstChild.firstChild;
 	scrollingElement.scrollTop = 0;
-	
+
 	document.getElementById("enter").play();
 	currentPost = postID;
 }
@@ -67,7 +67,7 @@ var setAttempts = function(){
 	var content = attempts + " Attempt(s) Left:";
 	for (i = 0; i < attempts; i++){
 		content += " ██";
-	} 
+	}
 	document.getElementById("attemptsleft").innerHTML = content;
 	if(attempts <= 1){
 		document.getElementById("message").innerHTML = "!!! WARNING: LOCKOUT IMMINENT !!!";
@@ -82,7 +82,7 @@ var setAttempts = function(){
 		addFeedback(">progress.");
 		setTimeout(function() {
 			document.getElementById("locked").play();
-			document.getElementById("hacking").innerHTML = 
+			document.getElementById("hacking").innerHTML =
 			"<p id=\"splashpage\">TERMINAL LOCKED<br><br>PLEASE CONTACT AN ADMINISTRATOR</p>";
 		}, 2000);
 	}
@@ -114,15 +114,7 @@ var clicked = function(span){
 			addFeedback(">Please wait");
 			addFeedback(">while system");
 			addFeedback(">is accessed.");
-			setTimeout(function() {
-				document.getElementById("hacking").style.display="none";
-				document.getElementById("loggedin").style.display="block";
-				window.addEventListener("load", resizeLoggedIn, false);
-				window.addEventListener("resize", resizeLoggedIn, false);
-				resizeLoggedIn();
-				//Play login sound
-				document.getElementById("login").play();
-			}, 2000);
+			setTimeout(login, 2000);
 		}
 	}else if(span.className == "symbol"){
 		var symbol = span.innerHTML;
@@ -138,14 +130,14 @@ var clicked = function(span){
 			content += children[i].innerHTML;
 		}
 		addFeedback(">" + content);
-	
+
 		//Make the brackets no longer clickable
 		span.removeAttribute("onclick");
 		var firstChild = span.firstElementChild;
 		firstChild.onclick = function(){clicked(firstChild)};
 		hovercleanup();
 		clickedBrackets.add(firstChild);
-		
+
 		//Choose between replenishing and removing dud
 		if(!hadRefresh && generateRandomInt(0,10) > 7){
 			//Replenish Tries
@@ -156,14 +148,14 @@ var clicked = function(span){
 			addFeedback(">replenished.");
 		}else{
 			//Remove Dud
-		
+
 			//Select the dud word to remove
 			var selectedWord = generateRandomInt(0, words.length);
 			var dudWord = words[selectedWord];
 			words.splice(selectedWord, 1);
-		
+
 			//Remove the dud word.
-			document.getElementById(dudWord).innerHTML = 
+			document.getElementById(dudWord).innerHTML =
 			document.getElementById(dudWord).innerHTML.replace(/[A-Z]/g,".");
 			addFeedback(">Dud removed.");
 		}
@@ -178,7 +170,7 @@ var convertToHex = function(d){
 	}
 	return "0x" + hex;
 }
-	
+
 var addBreakIfNeeded = function(string, count){
 	if(count != 0 && count % 12 == 0){
 		string += "<BR/>";
@@ -195,7 +187,7 @@ var addWord = function(string, word, count){
 		count++;
 		string += " ";
 		if(i+1 == word.length){
-			string += "</span>";	
+			string += "</span>";
 		}
 	}
 	return [count, string];
@@ -229,7 +221,7 @@ var detectClosingBracket = function(span){
 	}
 	//Recursively look at next elements hoping for a closing bracket
 	//If we hit a break or a letter (word), we didn't find one.
-	
+
 	//An array to hold the elements we looked at
 	var arr = [span];
 	do {
@@ -254,20 +246,20 @@ var hovercleanup = function() {
 	var parent = spantodelete.parentNode;
 	var ugly_children = spantodelete.childNodes;
 	var pretty_children = [];
-	
+
 	//JavaScript doesn't have a clone feature... -_-
 	//We copy to a new array to avoid changes affecting our array
 	for (var i = 0; i < ugly_children.length; i++) {
 		var child = ugly_children[i];
 		pretty_children.push(child);
 	}
-	
+
 	//Now we move children of newspan into the parent.
 	for (var i = 0; i < pretty_children.length; i++) {
 		var child = pretty_children[i];
 		spantodelete.removeChild(child);
 		parent.insertBefore(child, spantodelete);
-		
+
 	}
 	parent.removeChild(spantodelete);
 	spantodelete = null;
@@ -277,12 +269,12 @@ var hoversym = function(span) {
 	var open_regex = /&lt; |[{\[\(] /g;
 	//If touch an opening bracket
 	if (span.innerHTML.match(open_regex) && !clickedBrackets.has(span)){
-		
+
 		var returned = detectClosingBracket(span);
 		var parent = span.parentNode;
 		if(returned){
 			span.removeAttribute("onclick");
-			
+
 			var newspan = document.createElement("SPAN");
 			newspan.className = "bracketpair";
 			newspan.onclick = function(){clicked(newspan)};
@@ -301,7 +293,7 @@ var hoversym = function(span) {
 //Generate the hex pointers on either side of the symbols
 var generatePointerColumn = function(value) {
 	var string = "";
-	
+
 	var count = 0;
 	do{
 		string += convertToHex(value) + "<BR/>";
@@ -323,31 +315,31 @@ var generateSymbolColumn = function() {
 	var count = 0;
 	var numsymbols = 0;
 	while (count < 17*12) {
-		string += "<span class='symbol' onmouseover=\"hoversym(this)\" onclick=\"clicked(this)\">" 
+		string += "<span class='symbol' onmouseover=\"hoversym(this)\" onclick=\"clicked(this)\">"
 		+ symbols[generateRandomInt(0,symbols.length)] + " </span>";
 		count++;
 		numsymbols++;
-		
+
 		//Choose to add a word
-		if((count + wordslength < 17*12) 
+		if((count + wordslength < 17*12)
 			&& (numsymbols > 4)
 			&& (numsymbols == 45 || (generateRandomInt(0,17) > 15))){
-			
+
 			//Select a random word
 			var wordpos = generateRandomInt(0,array.length);
 			var word = array[wordpos];
-			
+
 			//Remove the element from the array
 			array.splice(wordpos, 1);
-			
+
 			//Store the word with the other words in a list.
 			words.push(word);
-	
+
 			var returned = addWord(string, word, count);
 			string = returned[1];
 			count = returned[0];
 			numsymbols = 0;
-			
+
 		}
 		string = addBreakIfNeeded(string, count);
 	}
@@ -379,21 +371,27 @@ var fixImages = function(){
 	}
 }
 
+login = function() {
+	document.getElementById("hacking").style.display="none";
+	document.getElementById("loggedin").style.display="block";
+	window.addEventListener("load", resizeLoggedIn, false);
+	window.addEventListener("resize", resizeLoggedIn, false);
+	resizeLoggedIn();
+	//Play login sound
+	document.getElementById("login").play();
+}
+
 window.onload = function(){
 	//Generate the pointers
 	var value = Math.floor(Math.random() * 65128);
-	var pcolumn = generatePointerColumn(value);
-	document.getElementById("leftpointers").innerHTML = pcolumn;
-	var pcolumn = generatePointerColumn(value+204);
-	document.getElementById("rightpointers").innerHTML = pcolumn;
+	document.getElementById("leftpointers").innerHTML = generatePointerColumn(value);
+	document.getElementById("rightpointers").innerHTML = generatePointerColumn(value+204);
 
-	//Generate the symbols 
-
+	//Generate the symbols
 	//We should have the array 'five' loaded in, which contains every 5 lettered english word.
-	var column = generateSymbolColumn();
-	document.getElementById("leftsymbols").innerHTML = column;
-	var column = generateSymbolColumn();
-	document.getElementById("rightsymbols").innerHTML = column;
+	//TODO: Replace 'five' with heroku node
+	document.getElementById("leftsymbols").innerHTML = generateSymbolColumn();
+	document.getElementById("rightsymbols").innerHTML = generateSymbolColumn();
 
 	//Select the goal word
 	var selectedWord = generateRandomInt(0, words.length);
@@ -405,5 +403,5 @@ window.onload = function(){
 	resizeLoggedIn();
 
 	fixImages();
-	
+
 }
