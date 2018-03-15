1c49e04886d798d9910fff0ed26cef41e3016053
Jetroid <holtjet@gmail.com>
2018-03-15 01:26:31 +0000

    Highlight now scroll based for touch, hover for pointer

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -1,11 +1,10 @@
-function getElementsInArea(e){
+function getElementsInArea(e) {
     var documentElement = document.documentElement;
     var viewportHeight = documentElement.clientHeight;
 
     var elements = document.querySelectorAll('.post-shadow');
-    for(var i = 0; i < elements.length; i++){
-        var element = elements[i];
-        var rect = element.getBoundingClientRect();
+    for (var i = 0; i < elements.length; i++) {
+        var rect = elements[i].getBoundingClientRect();
 
         //Calculate the position of top and bottom of element,
         //then calculate the middle of it
@@ -16,10 +15,25 @@ function getElementsInArea(e){
         //If the middle of our element is between 32 from top and 32 from bottom,
         //Give it a shadow.
         var inViewport = middle > 32 && middle < 68;
-        element.classList.toggle('shadow', inViewport);
+        elements[i].classList.toggle('shadow', inViewport);
     }
 }
 
-window.addEventListener('touchmove', getElementsInArea);
-window.addEventListener('scroll', getElementsInArea);
-window.addEventListener('resize', getElementsInArea);
+//We want to do scroll-based highlighting if touchscreen
+//or hover-based highlighting otherwise.
+//Condition detects touch device
+//https://stackoverflow.com/questions/4817029
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    window.addEventListener('touchmove', getElementsInArea);
+    window.addEventListener('scroll', getElementsInArea);
+    window.addEventListener('resize', getElementsInArea);
+} else {
+    var elements = document.querySelectorAll('.post-shadow');
+    for (var i = 0; i < elements.length; i++) {
+        (function(){
+            var element = elements[i];
+            element.onmouseenter = function(){element.classList.toggle('shadow',true);};
+            element.onmouseleave = function(){element.classList.toggle('shadow',false);};
+        }());
+    }
+}
